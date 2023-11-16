a8e5c3d2bf502c095b7f2eb8bf5a3f3424b59369
Mohammed El Mouden <mohamed55mouden@gmail.com>
2023-11-16 19:55:39 +0100

    navbar set up , searsh active button when lick on icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,48 @@
 // Navbar.tsx
 import React from "react";
 import { Link } from "react-router-dom"; // Assuming you're using React Router
+import "../styles/navbar.css";
+import logo from "../assets/GoodiesLogo.svg";
+import { FaSearch, FaShoppingBag } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar: React.FC = () => {
+  const [showSearchInput, setShowSearchInput] = useState(false);
+
+  const toggleSearchInput = () => {
+    setShowSearchInput(!showSearchInput);
+  };
   return (
     <nav>
-      <div className="navbar-container">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        {/* Add more links as needed */}
-
-        {/* You can also add login/logout logic based on user authentication */}
-        {/* For example, display "Login" if the user is not logged in, and "Logout" if they are */}
-        <Link to="/login">Login</Link>
+      <div className="logo">
+        <img src={logo} alt="Goodies" />
       </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/main">Main</Link>
+        </li>
+        <li>
+          <Link to="/">About</Link>
+        </li>
+        <li>
+          <Link to="/">Services</Link>
+        </li>
+        <li>
+          <Link to="/">Contact</Link>
+        </li>
+      </ul>
+      <form className="search-form">
+        <div className="search-icons">
+          <FaSearch onClick={toggleSearchInput} />
+          <FaShoppingBag />
+        </div>
+        <input
+          type="text"
+          placeholder="Search..."
+          className={showSearchInput ? "active" : ""}
+        />
+        
+      </form>
     </nav>
   );
 };
